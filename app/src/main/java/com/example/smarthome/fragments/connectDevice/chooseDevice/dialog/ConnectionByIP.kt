c5adf29982790ad7a6ca410cc644ea97fbc0e81e
dm1tryM <108665757+dm1tryM@users.noreply.github.com>
2023-03-26 14:53:07 +0300

    Чистка импортов

--- app/src/main/java/com/example/smarthome/fragments/connectDevice/chooseDevice/dialog/ConnectionByIP.kt
+++ app/src/main/java/com/example/smarthome/fragments/connectDevice/chooseDevice/dialog/ConnectionByIP.kt
@@ -1,13 +1,9 @@
 package com.example.smarthome.fragments.connectDevice.chooseDevice.dialog
 
 import android.app.Dialog
-import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
-import com.example.smarthome.R
 import com.example.smarthome.databinding.DropmenuConnectWifiDeviceByIpBinding
 import com.example.smarthome.core.utils.BottomSheetDialogBuilder
-import com.example.smarthome.core.utils.isIpAddress
-import com.example.smarthome.core.utils.snackBar
 
 object ConnectionByIP {
     fun create(
