1d106a802bb054eaa817f5c55574d94c03a2995a
dm1tryM <108665757+dm1tryM@users.noreply.github.com>
2023-04-02 20:12:52 +0300

    Чистка кода

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,6 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.JetBrains.kotlin}")
         classpath(Deps.kotlinXSerialization)
     }
 }
