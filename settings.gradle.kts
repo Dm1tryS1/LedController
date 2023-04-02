c54591c29223cda3e0cff6891982b3008f3622b1
dm1tryM <108665757+dm1tryM@users.noreply.github.com>
2023-04-02 20:03:38 +0300

    Чистка кода

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,3 @@
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-        maven(url = "https://jitpack.io")
-    }
-}
-
 rootProject.name = "SmartHome"
 include(":app")
 include(":common:core")
